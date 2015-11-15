cf975342249224a2aca8871167d11384bc44a473
yanhuangzaixian <liyanhuang.france@gmail.com>
2015-11-15 23:46:52 +0100

    version 34 15/11/2015

---
+++ src/otherTest/testSendOneCommandFunction.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package otherTest;
+
+import com.jcraft.jsch.JSchException;
+import java.io.IOException;
+import util.allocation;
+
+/**
+ *
+ * @author RDSG6431
+ */
+public class testSendOneCommandFunction {
+    
+    
+    public static void main(String[] arg) throws IOException, JSchException{
+    
+        String returnInfo=allocation.createVMAndVolumeOnSP("outVM", 1, "yli03", "VJWWFDRA", "127.0.0.1", 2222);
+        
+        String volumeId=allocation.getVolumeIdFromString(returnInfo);
+        
+        allocation.attachVolumeToVM("outVM", volumeId, "yli03", "VJWWFDRA", "127.0.0.1", 2222);
+        
+        
+    }
+    
+    
+}
--- src/util/allocation.java
+++ src/util/allocation.java
@@ -22,6 +22,14 @@ import static util.method.getVMByID;
 import static util.method.printInfo;
 import static util.method.printLinkedList;
 import static util.method.twoLinkedListShareSameEntity;
+import com.jcraft.jsch.*;
+import java.awt.*;
+import java.io.*;
+import javax.swing.*;
+import org.apache.commons.io.IOUtils;
+import util.allocation;
+import util.method;
+import static util.method.printInfo;
 
 /**
  *
@@ -319,5 +327,282 @@ public class allocation {
        return ID;
    
     }
+   
+   
+   public static String createVMAndVolumeOnSP(String VMID,int volume, String user, String passWord, String host, int port) throws JSchException, IOException
+   {
+     JSch jsch=new JSch();  
+
+      
+  
+
+      Session session=jsch.getSession(user, host, port);
+      
+     
+      // username and password will be given via UserInfo interface.
+      UserInfo ui=new MyUserInfo(passWord);
+      session.setUserInfo(ui);
+      
+      session.connect();
+
+      
+      String command0="pwd";
+      String command1="cd devstack";
+      String command2="source openrc admin admin";
+      String command3="nova boot --flavor 1  --image 443880f8-84bc-4077-86ca-d8dd8ae69289"+" "+VMID;
+      String command4="nova volume-create"+" "+volume+" "+"--display_name outVol1";
+     
+      //String command5="cd devstack && source openrc admin admin && nova boot --flavor 1  --image 443880f8-84bc-4077-86ca-d8dd8ae69289  outVM1 && nova volume-create 1 --display_name outVol1 ";
+            
+      String command5=command1+" && "+command2+" && "+command3+" && "+command4;
+
+      Channel channel=session.openChannel("exec");
+      
+      
+      
+      ((ChannelExec)channel).setCommand(command5);
+      
+      
+      
+      
+      
+      
+     
+      
+      channel.setInputStream(null);
+
+
+      ((ChannelExec)channel).setErrStream(System.err);
+
+      InputStream in=channel.getInputStream();
+
+      channel.connect();
+
+      
+     
+      
+      
+        StringWriter writer = new StringWriter();
+        IOUtils.copy(in, writer, "UTF-8");
+        String returnInfo = writer.toString();
+        
+        System.out.println(returnInfo);
+        
+          ((ChannelExec)channel).setErrStream(System.err);
+          
+        printInfo("flag0");
+         
+        channel.disconnect();
+        session.disconnect();
+        
+        return returnInfo;
+   
+   }
+   
+   
+   
+   
+    public static void attachVolumeToVM(String VMID,String volumeID, String user,String passWord, String host, int port) throws JSchException, IOException
+   {
+     JSch jsch=new JSch();  
+
+      
+  
+
+      Session session=jsch.getSession(user, host, port);
+      
+     
+      // username and password will be given via UserInfo interface.
+      UserInfo ui=new MyUserInfo(passWord);
+      session.setUserInfo(ui);
+      
+      session.connect();
+
+      
+      String command0="pwd";
+      String command1="cd devstack";
+      String command2="source openrc admin admin";
+     
+      String command6="nova volume-attach"+" "+VMID+" "+volumeID+" /dev/vdb";
+        
+        
+     
+       //String command7="pwd";
+      String command7=command1+" && "+command2+" && "+command6;
+
+       printInfo(command7);
+      
+      Channel channel=session.openChannel("exec");
+      
+      
+      
+      ((ChannelExec)channel).setCommand(command7);
+      
+      
+      
+      
+      
+      
+     
+      
+      channel.setInputStream(null);
+
+
+      ((ChannelExec)channel).setErrStream(System.err);
+
+      InputStream in=channel.getInputStream();
+
+      channel.connect();
+
+      
+     
+      
+      
+        StringWriter writer = new StringWriter();
+        IOUtils.copy(in, writer, "UTF-8");
+        String returnInfo = writer.toString();
+        
+        System.out.println(returnInfo);
+        
+          ((ChannelExec)channel).setErrStream(System.err);
+          
+        printInfo("flag0");
+         
+        channel.disconnect();
+        session.disconnect();
+        
+        
+   
+   }
+   
+   
+   
+   
+   
+   public static class MyUserInfo implements UserInfo, UIKeyboardInteractive{
+    
+ 
+      
+      
+      
+    String passwd;
+    JTextField passwordField=(JTextField)new JPasswordField(20);  
+      
+      
+         
+    MyUserInfo(String password)
+    {
+      this.passwd=password;
+    }
+    
+    
+    public String getPassword(){ return passwd; }
+    
+    
+    public boolean promptYesNo(String str){
+        
+      /*  
+      Object[] options={ "yes", "no" };
+      int foo=JOptionPane.showOptionDialog(null, 
+             str,
+             "Warning", 
+             JOptionPane.DEFAULT_OPTION, 
+             JOptionPane.WARNING_MESSAGE,
+             null, options, options[0]);
+       return foo==0;
+       */
+        
+        
+       return true;
+       
+    }
+  
+    
+
+    public String getPassphrase(){ return null; }
+    
+    public boolean promptPassphrase(String message){ return true; }
+    
+    public boolean promptPassword(String message){
+        /*
+      Object[] ob={passwordField}; 
+      int result=
+        JOptionPane.showConfirmDialog(null, ob, message,
+                                      JOptionPane.OK_CANCEL_OPTION);
+      if(result==JOptionPane.OK_OPTION){
+        passwd=passwordField.getText();
+        return true;
+      }
+      else{ 
+        return false; 
+      }*/
+        
+        
+        return true;
+    }
+    
+    
+    public void showMessage(String message){
+      JOptionPane.showMessageDialog(null, message);
+    }
+    final GridBagConstraints gbc = 
+      new GridBagConstraints(0,0,1,1,1,1,
+                             GridBagConstraints.NORTHWEST,
+                             GridBagConstraints.NONE,
+                             new Insets(0,0,0,0),0,0);
+    private Container panel;
+    public String[] promptKeyboardInteractive(String destination,
+                                              String name,
+                                              String instruction,
+                                              String[] prompt,
+                                              boolean[] echo){
+      panel = new JPanel();
+      panel.setLayout(new GridBagLayout());
+
+      gbc.weightx = 1.0;
+      gbc.gridwidth = GridBagConstraints.REMAINDER;
+      gbc.gridx = 0;
+      panel.add(new JLabel(instruction), gbc);
+      gbc.gridy++;
+
+      gbc.gridwidth = GridBagConstraints.RELATIVE;
+
+      JTextField[] texts=new JTextField[prompt.length];
+      for(int i=0; i<prompt.length; i++){
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.gridx = 0;
+        gbc.weightx = 1;
+        panel.add(new JLabel(prompt[i]),gbc);
+
+        gbc.gridx = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weighty = 1;
+        if(echo[i]){
+          texts[i]=new JTextField(20);
+        }
+        else{
+          texts[i]=new JPasswordField(20);
+        }
+        panel.add(texts[i], gbc);
+        gbc.gridy++;
+      }
+
+      if(JOptionPane.showConfirmDialog(null, panel, 
+                                       destination+": "+name,
+                                       JOptionPane.OK_CANCEL_OPTION,
+                                       JOptionPane.QUESTION_MESSAGE)
+         ==JOptionPane.OK_OPTION){
+        String[] response=new String[prompt.length];
+        for(int i=0; i<prompt.length; i++){
+          response[i]=texts[i].getText();
+        }
+	return response;
+      }
+      else{
+        return null;  // cancel
+      }
+    }
+  }
+   
    
 }
